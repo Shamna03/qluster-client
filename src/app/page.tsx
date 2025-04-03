dd38e611c44b821d4f1b0ae97eca75886d2bc700
muhammedajmalkt <muhammedajmalkt@gmail.com>
2025-04-03 15:50:18 +0530

    changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex justify-center items-center h-screen ">
-
      <h1 className="text-red-300 ">Weolcome to qluster</h1>
       
     </div>
